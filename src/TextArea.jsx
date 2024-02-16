56279711fcc49160e88c5552b8767ad568314bc4
Marc Bäckman <93243973+mabackma@users.noreply.github.com>
2024-02-16 20:19:47 +0200

    added translate to finnish

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -4,6 +4,7 @@ import './TextArea.css';
 const TextArea = () => {
   const [value, setValue] = useState('');
   const [summary, setSummary] = useState('');
+  const [translation, setTranslation] = useState('');
 
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -13,7 +14,7 @@ const TextArea = () => {
     // Create a JSON object with the text area value
     const data = { text: value };
 
-    // Send a POST request to the web address
+    // Send a POST request to the summary address
     fetch('http://127.0.0.1:5000/summary', {
       method: 'POST',
       headers: {
@@ -29,6 +30,28 @@ const TextArea = () => {
     })
     .then(data => {
       setSummary(data.summary);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      // Handle errors here
+    });
+
+    // Send a POST request to the translate address
+    fetch('http://127.0.0.1:5000/translate-to-finnish', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      setTranslation(data.translation);
     })
     .catch(error => {
       console.error('Error:', error);
@@ -51,9 +74,17 @@ const TextArea = () => {
       <br/>
       <button onClick={handleClick}>Send Text</button>
       {
+        translation ?
+        <div className="summary-container">
+            <label>Translation to finnish from input text:</label>
+            <p>{translation}</p>
+        </div>
+        : null
+      }
+      {
         summary ?
         <div className="summary-container">
-            <label>Summary:</label>
+            <label>Summary from input text:</label>
             <p>{summary}</p>
         </div>
         : null
