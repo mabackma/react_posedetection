10d169947d3c63faaf42d13531868069eae820e7
Marc Bäckman <93243973+mabackma@users.noreply.github.com>
2024-02-26 17:15:37 +0200

    removed text area and added squatting model

--- src/SelectModel.jsx
+++ src/SelectModel.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import TMModelLoad from "./TMModelLoad";
-import TextArea from "./TextArea";
 
 function SelectModel(){
     
@@ -24,8 +23,7 @@ function SelectModel(){
     return (
         <div id="select-model-buttons-div">
             <button id="model1-button" onClick={handleClickModelButton}>Load Model 1</button>
-            {showModel && <TMModelLoad path_to_model={get_path_to_model(2)} path_to_metadata={get_path_to_metadata(2)}/>}
-            <TextArea/>
+            {showModel && <TMModelLoad path_to_model={get_path_to_model(3)} path_to_metadata={get_path_to_metadata(3)}/>}
         </div>
     )
 }
